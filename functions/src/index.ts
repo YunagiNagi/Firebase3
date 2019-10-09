a199838ff1f57ee7f04290c383deb6b70eec6d16
YunagiNagi <ryoshi0412@gmail.com>
2019-10-09 22:59:52 +0900

    add function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,9 +11,23 @@ admin.initializeApp({
 });
 
 export const helloWorld = functions.https.onRequest(
-    async(request, response) => {
-        const original = request.query.text;
+  async(request, response) => {
+      const original = request.query.text;
+      if (original) {
         const snapshot = await admin.database().ref('/messages').push({original: original});
         response.redirect(303, snapshot.ref.toString());
-    }
+      } else {
+        response.status(200).send("");
+      }
+  }
+);
+
+export const makeUppercase = functions.database.ref('/messages/{pushId}/original').onCreate(
+  async (snapshot, context) => {
+    const original = snapshot.val();
+    const uppercase = original.toUpperCase();
+    const parent = snapshot.ref.parent;
+    if (parent) return parent.child('uppercase').set(uppercase);
+    else return null;
+  }
 );
